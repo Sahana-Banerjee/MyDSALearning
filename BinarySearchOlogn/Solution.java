949767d46b8ebd3c46c5456b7f03846d1e76a5c1
Sahana-Banerjee <132974452+Sahana-Banerjee@users.noreply.github.com>
2023-05-29 04:09:37 +0000

    Rectified Binary Search Code

--- BinarySearchOlogn/Solution.java
+++ BinarySearchOlogn/Solution.java
@@ -1,26 +1,30 @@
 class Solution {
   public static int search(int[] nums, int target, int sidx, int lidx) {
-    int pidx = (sidx + lidx) / 2;
+    if (lidx >= sidx) {
+      int pidx = (sidx + lidx) / 2;
 
-    if (nums[pidx] == target) {
-      return pidx;
-    }
-    if (target < nums[pidx]) {
-      return search(nums, target, 0, pidx - 1);
-    } else if (target > nums[pidx]) {
-      return search(nums, target, pidx + 1, nums.length - 1);
+      if (nums[pidx] == target) {
+        return pidx;
+      }
+      if (target < nums[pidx]) {
+        System.out.println("ifff" + pidx);
+        return search(nums, target, 0, pidx - 1);
+      } else if (target > nums[pidx]) {
+        return search(nums, target, pidx + 1, lidx);
+      }
     }
     return -1;
   }
 
   public static void main(final String[] args) {
-    int nums[] = { -1, 0, 3, 5, 9, 12 };
-    int target = 2;
+    int nums[] = { 1, 6, 8, 11, 15 };
+    int target = -1;
     int nlen = nums.length;
     int startIdx = 0;
-    // int piIdx = nlen / 2 - 1;
     int found = search(nums, target, startIdx, nlen - 1);
     System.out.println("Found at : " + found);
   }
+
+  // This has the best time complexity of O(1)
 
 }
